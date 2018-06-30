265275403b73428ef9106ec82c36f009a195cb56
grey-zeng <zengqinxiong@outlook.com>
2018-06-30 21:13:35 +0800

    整理初始代码

---
+++ base/debug.cc
@@ -0,0 +1,20 @@
+/*
+ * @Author: zengqinxiong 
+ * @Date: 2018-06-30 20:29:27 
+ * @Last Modified by: zengqionxiong
+ * @Last Modified time: 2018-06-30 20:30:54
+ */
+#include <iostream>
+using namespace std;
+
+
+int main(int argc, char const *argv[])
+{
+    int sum = 0;
+    for (int i = 0;i < 10; i++)
+    {
+        sum += i;
+        cout << sum << endl;   
+    }
+    return 0;
+}
